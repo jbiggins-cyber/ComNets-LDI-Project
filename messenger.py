ef05ae69c8cac4b43a5bc4523d5ff99990d89b9c
Alexander Leris <alexander.leris@gmail.com>
2023-10-06 17:46:18 +1100

    Start bringing RDT functions into messenger class

--- messenger.py
+++ messenger.py
@@ -1,4 +1,5 @@
 from transport import *
+from rdt_protocol import *
 
 # I think this needs to be changed somewhat
 # once send is called, the whole Messenger API should block until the correct ACKs (or whatever the protocol dictates) have been received
@@ -6,8 +7,23 @@ from transport import *
 #   Unsure how to have this call pend til then? Maybe the whole lower level will allow us to pend?
 
 
+
+"""
+messenger child classes need to define
+- send (implement fsm)
+- receive (implement fsm)
+- _create_header
+- _parse_header
+"""
+
 class Messenger():
     """The Messenger class manages communication using a custom designed protocol"""
+
+    FLAGS = {"ACK": 0x01, "FIN": 0x02, "NACK": 0x04}
+    N_FLAG_HEXS  = 2
+    N_SEQ_DIGITS = 4
+    N_CHECKSUM_CHARS = 7
+    PACKET_DATA_LEN = 100 # bytes -- todo change
 
     def __init__(self, sock_type: str, ip: str):
         self.ip = ip
@@ -19,6 +35,9 @@ class Messenger():
 
     def send(self, data: str):
         """Format this data with our own RDP protocol, then send over the lower-level base"""
+
+        # send_buffer = self._split_data_into_packets(data)
+
         send_buffer = "HEADER D1 D2 D3\n"+data
         self.transport.send(send_buffer)
 
@@ -26,15 +45,7 @@ class Messenger():
         """Parse out the RDP protocol and just return our data"""
         recv_buffer = self.transport.receive()
         
-        # in the first draft, header will be separated from data by a newline
-        # there should be a better way to do this
-        try:
-            header_end = recv_buffer.index('\n')
-            header = recv_buffer[:header_end]
-            data = recv_buffer[header_end+1:]
-        except ValueError:
-            header = recv_buffer
-            data = ""
+        header, data = self.__get_header_data_split(recv_buffer)
 
         print("Received Messenger comms:\n" + "\tHeader: " + header + "\n\tData: " + data + "\n------")
         return data
@@ -43,50 +54,40 @@ class Messenger():
         self.send("FINMSG")
         self.transport.close()
 
-class ClientMessenger(Messenger):
-    def __init__(self, ip: str):
-        super().__init__('client', ip)
-        self._get_new_sock()
+    def _split_data_into_packets(self, data: str) -> list[str]:
+        """
+        Split up a message by size
+        This does the make_pkt() functionality
+        """
 
-class ServerMessenger(Messenger):
-    def __init__(self, ip:str):
-        super().__init__('server', ip)
-        self._get_new_sock()
-
-
-
-#### TODO: THIS CLASS NEEDS A CLEANUP, A REVIEW OF API, AND PERHAPS A RENAMING
-# IDEALLY, THERE WOULD BE SOME WAY FOR IT TO BE INTEGRATED INTO THE MESSENGER CLASS
-# POTENTIALLY, ALL MESSENGER CLASS PACKETS COULD SUPPORT MOST OF THIS FUNCTIONALITY 
-# THEMSELVES, AND IT'S UP TO THE PROTOCOL TO SELECT WHICH HEADERS TO USE. I'M UNSURE
-# WHAT SHOULD BE DONE WITH THINGS LIKE THE ACK STATES, RETRANSMISSIONS, ETC.
-
-class Protocol():
-
-    FLAGS = {"ACK": 0x01, "FIN": 0x02, "NACK": 0x04}
-    N_FLAG_HEXS  = 2
-    N_SEQ_DIGITS = 4
-    PACKET_DATA_LEN = 100 # bytes -- todo change
-
-    def __init__(self):
-        self.seq = 0
-        self.send_buffer = []
-
-    def enqueue_packets(self, flags: int, data: str):
-        """Split up a message by size and append the required headers"""
         data_idx = 0
+        packet_list = []
+        seq = 0
         while len(data[data_idx:]) > self.PACKET_DATA_LEN:
-            header_params = {"seq": self.seq, "flags": 0x00}
-            next_packet = self.__create_header(header_params) + "\n" + data[data_idx:min(data_idx+self.PACKET_DATA_LEN, len(data))]
-            self.send_buffer.append(next_packet)
-            
-    def parse_packet(self, packet: str) -> tuple[dict[str, any], str]:
-        """Parse a received packet into its params and data"""
+            header_params = {"seq": seq, "flags": 0x00, "Check": 0}
+            header = self._create_header(header_params)
+            next_packet = data[data_idx:min(data_idx+self.PACKET_DATA_LEN, len(data))]
+            packet_list.append(next_packet)
+            seq += 1
+        # todo fix indexing so we don't need to do again once the while loop finishes
+        header_params = {"seq": seq, "flags": 0x00, "Check": 0}
+        header = self._create_header(header_params)
+        next_packet = data[data_idx:min(data_idx+self.PACKET_DATA_LEN, len(data))]
+        packet_list.append(next_packet)
+        return packet_list
+
+    def _extract(self, packet: str) -> tuple[dict[str, any], str]:
+        """
+        Parse a received packet into its params and data
+        This is the extract() function
+        """
         header, data = self.__get_header_data_split(packet)
-        params = self.__parse_header(header)
+        params = self._parse_header(header)
         return params, data
-    
-    def __parse_header(self, header: str) -> dict[str, int]:
+
+
+    # OVERRIDE IN CHILD 
+    def _parse_header(self, header: str) -> dict[str, int]:
         """Take a header string and parse out the seq num, flags, (any other data we add in the future)"""
         i = header.index('S:')
         seq_num = int(header[i+len('S:'):i+self.N_SEQ_DIGITS+len('S:')])
@@ -94,16 +95,29 @@ class Protocol():
         i = header.index('F:')
         flags = int(header[i+len('F:'):i+self.N_FLAG_HEXS+len('F:')])
 
-        return {"seq": seq_num, "flags": flags}
+        i = header.index('C:')
+        checksum = int(header[i+len('F:'):i+self.N_CHECKSUM_CHARS+len('F:')])
 
-    def __create_header(self, params: dict[str, any]) -> str:
-        """Create the procotol header for given params
-        Current params: `seq`, `flags`"""
+        return {"seq": seq_num, "flags": flags, "check": checksum}
+
+    # OVERRIDE IN CHILD
+    def _create_header(self, params: dict[str, any]) -> str:
+        """
+        Create the procotol header for given params
+        Current params: `seq`, `flags`, `check`
+        """
+        if not 'seq' in params:
+            raise ValueError("Missing sequence number (key: 'seq')")
         if not (0 <= params["seq"] <= 9999):
             raise ValueError(f"Seq num out of range: {seq}")
+        if not 'flags' in params:
+            raise ValueError("Missing flags (key: 'flags')")
         if not (0x00 <= params["flags"] <= 0xFF):
             raise ValueError(f"Flags out of range: {flags}")
-        return f"HEADER S:{params['seq']:04d} F:{params['flags']:02x}"
+        if not 'check' in params:
+            raise ValueError("Missing checksum (key: 'check')")
+        # todo figure out checksum length! 7 is default git short length
+        return f"HEADER S:{params['seq']:04d} F:{params['flags']:02x} C:{params['check'][0:N_CHECKSUM_CHARS]}"
 
     def __get_header_data_split(self, buffer: str) -> tuple[str, str]:
         """Split a received buffer into header and data components"""
@@ -116,3 +130,14 @@ class Protocol():
             data = ""
 
         return header, data
+
+
+class ClientMessenger(Messenger):
+    def __init__(self, ip: str):
+        super().__init__('client', ip)
+        self._get_new_sock()
+
+class ServerMessenger(Messenger):
+    def __init__(self, ip:str):
+        super().__init__('server', ip)
+        self._get_new_sock()
