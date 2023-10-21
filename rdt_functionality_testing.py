cf03a901bf61dca51ea416cccb79c424c67aef22
Joshua Biggins <82558048+jbiggins-cyber@users.noreply.github.com>
2023-10-21 13:29:35 +1100

    Refactored code and files names.
    Divided functions into public and private so Alex can easily see which ones are relevant. Renamed rdt.py to rdt_functionality.py

---
+++ rdt_functionality_testing.py
@@ -0,0 +1,47 @@
+from rdt_functionality import *
+
+print()
+message = "This is a test message string!"
+print("Original message: {}".format(message))
+unicodeMessage = str2Bin(message)
+print("Message to binary: {}".format(''.join(unicodeMessage)))
+decodedMessage = bin2Str(unicodeMessage)
+print("Binary to string: {}".format(decodedMessage))
+
+print()
+byteArr = message.encode('utf-8')
+print("Original message to bytes: {}".format(byteArr))
+unicodeBytes = bytes2Bin(byteArr)
+print("Bytes to binary: {}".format(''.join(unicodeBytes)))
+decodedBytesMessage = bin2Str(unicodeBytes)
+print("Decoded bytes payload: {}".format(decodedBytesMessage))
+
+print()
+corruptedBits = __corruptBits(unicodeMessage, 1)
+corruptedMessage = bin2Bytes(corruptedBits)
+print("Original message bytes:           {}".format(byteArr))
+print("Corrupted original message bytes: {}".format(corruptedMessage))
+
+print()
+checksum = __generateUDPChecksumBits(unicodeMessage)
+print("Checksum of original message: {}".format(checksum))
+print("Checksum verification of original message: {}".format(verifyUDPChecksum(unicodeMessage, checksum)))
+print("Checksum verification of corrupted message: {}".format(verifyUDPChecksum(corruptedBits, checksum)))
+
+print()
+bytesArray = __generate2DParityCheck(unicodeMessage)
+print("Bytes array: {}".format(bytesArray))
+sndrParity2DList = generate2DParityCheck(unicodeMessage)
+[correctedBits, success] = verify2DParityCheck(corruptedBits, sndrParity2DList)
+if success == SUCCESS: 
+    correctedMessage = bin2Str(correctedBits)
+    print("Corrected message: {}".format(correctedMessage))
+
+print()
+someBits = ['0', '1', '1', '0', '1', '0', '0', '1']
+print("Original message: {}".format(byteArr))
+burstedBytes = burstError(byteArr, 16)
+print("Burst error msg : {}".format(burstedBytes))
+
+print()
+print("Corrupted bytes: {}".format(corrupt(byteArr, 2)))
